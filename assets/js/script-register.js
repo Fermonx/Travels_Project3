6f5318ca99623725812593259a79d4e2d764148d
FernanMonc <fernandomdsatoutlook.com>
2018-03-21 15:27:17 +0100

    Finished register box, it checks for both passwords being the same, all fields required and the user must agree to the terms to be able to send the registration

--- assets/js/script-register.js
+++ assets/js/script-register.js
@@ -3,11 +3,24 @@
 $("#inputPassword2").keyup(function(){
     var pw1 = $("#inputPassword1").val().length;
     var pw2 = $("#inputPassword2").val().length;
+    var pw11 = $("#inputPassword1").val();
+    var pw22 = $("#inputPassword2").val();
 
-    if(pw1 != pw2)
+    if((pw1 !== pw2)&&(pw11 !== pw22))
     {
        $("#inputPassword2").addClass("form-control2");
     }
-    else if (pw2 == pw1)
+    else if ((pw2 === pw1)&&(pw22 === pw11))
         $("#inputPassword2").removeClass("form-control2");
+});
+
+$(function()
+{
+    $('#checkb').click(function()
+    {
+        if($(this).is(':checked'))
+            ($("#regbutton")).removeClass('disabled');
+        else
+            ($("#regbutton")).addClass('disabled');
+    });
 });
