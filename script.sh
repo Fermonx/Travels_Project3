041bee1ad7d6170f0d183ac00b958d3d5a0e8911
FernanMonc <fernandomdsatoutlook.com>
2018-03-24 19:21:11 +0100

    Finished installing vagrant with the script to pull/clone depending on the project existance in the container. It installs nodejs, nodejs-legacy and npm.

--- script.sh
+++ script.sh
@@ -1,33 +1,29 @@
-
-
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git
-apt-get install -y node
 apt-get install -y nodejs
 apt-get install -y nodejs-legacy
-#apt-get install -y mysql-server php5-mysql
-#debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-#debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
-#apt-get -f install
+apt-get install -y npm
+apt-get install -y git
+npm install forever
+apt-get install -y mysql-server php5-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+apt-get -f install
 
-if ! '[cd project]';
+if [[ $(cd project) ]]
 then
-    mkdir project
-    cd project
-    git clone https://github.com/Fermonx/actividad_3.git
-fi
+    git pull https://github.com/Fermonx/actividad_3.git
+
 else
-git pull
+mkdir project
+cd project || exit
+git clone https://github.com/Fermonx/actividad_3.git
 fi
 
-if ! [ -L /var/www/html/ ];
-then
+if ! [ -L /var/www/html/ ]; then
 sudo rm -rf /var/www/html/
 sudo ln -fs /vagrant /var/www/html
 fi
-
-
 
 
